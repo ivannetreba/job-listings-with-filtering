33b3764ba723406a419deab7b42155a73282a57d
ivannetreba <ivannetreba14@gmail.com>
2024-01-25 02:32:37 +0400

    Build desctop version for job container.

--- src/component/Job.tsx
+++ src/component/Job.tsx
@@ -5,25 +5,31 @@ interface Props {
 }
 
 const Job = ({ job }: Props) => {
+  const jobClasses = job.new ? "job job__highlight" : "job";
+
   return (
-    <article className="job">
-      <img className="job__logo" src={job.logo} alt={job.company} />
-      <div className="job__description-box">
-        <p className="job__company-line">
-          <span className="job__company">{job.company}</span>
-          {job.new && (
-            <span className="job__status job__status--cyan">NEW!</span>
-          )}
-          {job.new && (
-            <span className="job__status job__status--black">FEATURED</span>
-          )}
-        </p>
-        <p className="job__position">{job.position}</p>
-        <p className="job__details-box">
-          <span className="job__detail">{job.postedAt}</span>
-          <span className="job__detail">{job.contract}</span>
-          <span className="job__detail">{job.location}</span>
-        </p>
+    <article className={jobClasses}>
+      <div className="job__logo-description">
+        <img className="job__logo" src={job.logo} alt={job.company} />
+        <div className="job__description-box">
+          <p className="job__company-line">
+            <span className="job__company">{job.company}</span>
+            {job.new && (
+              <span className="job__status job__status--cyan">NEW!</span>
+            )}
+            {job.new && (
+              <span className="job__status job__status--black">FEATURED</span>
+            )}
+          </p>
+          <p className="job__position">{job.position}</p>
+          <p className="job__details-box">
+            <span className="job__detail">{job.postedAt}</span>
+            <div className="job__dot"></div>
+            <span className="job__detail">{job.contract}</span>
+            <div className="job__dot"></div>
+            <span className="job__detail">{job.location}</span>
+          </p>
+        </div>
       </div>
       <div className="job__requirements-box">
         <span className="job__requirement">{job.role}</span>
